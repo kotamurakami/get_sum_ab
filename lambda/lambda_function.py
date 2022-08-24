e3b35282c5700b9562e80021581e1632084e64ab
unknown <mrkm.kt.aug.31@gmail.com>
2022-08-24 16:08:18 +0900

    do testing

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,7 +2,9 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    body = json.loads(event['body'])
+    hoge = body['hoge']
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! updated..!')
+        'body': json.dumps(hoge)
     }
