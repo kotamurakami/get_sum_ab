821078d4bbaece1cc0a25c1bfac304afc5ca7908
unknown <mrkm.kt.aug.31@gmail.com>
2022-08-27 22:43:23 +0900

    just testing...

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,10 +2,10 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    body = json.loads(event['body'])
+    "#body = json.loads(event['body'])
     #hoge = body['hoge']
     return {
         'statusCode': 200,
         #'body': json.dumps(hoge)
-        'body': json.dumps(body)
+        'body': json.dumps(event['body'])
     }
