4c1615e252d04fa7d87dfff045df3490bcbd49bd
unknown <mrkm.kt.aug.31@gmail.com>
2022-12-28 09:54:03 +0900

    'test'

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     # TODO implement
     body = json.loads(event['body'])
     ans = body['a']
-    ans = ans + body['b']
+    #ans = ans + body['b']
     return {
         'statusCode': 200,
         'body': json.dumps(ans)
